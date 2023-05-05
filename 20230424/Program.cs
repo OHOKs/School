0299bf10966bcfebf5fddab9bec57342f9c5c386
OHOKs <85952515+OHOKs@users.noreply.github.com>
2023-05-05 11:59:43 +0200

    Add files via upload

---
+++ 20230424/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace asdasd
+{
+    class Fajl
+    {
+        static void Main(string[] args)
+        {
+            string inputFileName = "D://JP//asd//input.txt";
+            string outputFileName = "D://JP//output//rovid.csv";
+            if(!Directory.Exists("D://JP//output//")) Directory.CreateDirectory("D://JP//output//");
+            if (!File.Exists("D://JP//output//rovid.csv")) { var create = File.Create("D://JP//output//rovid.csv"); create.Close(); }
+
+            var lines = File.ReadAllLines(inputFileName).Where(arg => !string.IsNullOrWhiteSpace(arg));
+
+            using (StreamWriter outputFile = new StreamWriter(outputFileName))
+            {
+                foreach (var item in lines)
+                {
+                    string[] line = item.Split(';');
+
+                    outputFile.WriteLine($"{line[0]};{line[1]}");
+                }
+            }
+        }
+    }
+}
